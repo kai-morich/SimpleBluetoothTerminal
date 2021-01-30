594bdce19bf3540894c1afe9f17ff365a98b7e5e
LuFernandez <lu.gfernandez@gmail.com>
2021-01-30 20:36:26 -0300

    agrego un segundo gráfico más abajo

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/MainActivity.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/MainActivity.java
@@ -22,16 +22,25 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
         else
             onBackStackChanged();
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> test = new LineGraphSeries<DataPoint>();
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        GraphView ECGgraph = (GraphView) findViewById(R.id.ECGgraph);
+        LineGraphSeries<DataPoint> ECG_samples = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
-        graph.addSeries(series);
+        ECGgraph.addSeries(ECG_samples);
+
+        GraphView PPGgraph = (GraphView) findViewById(R.id.PPGgraph);
+        LineGraphSeries<DataPoint> PPG_samples = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        PPGgraph.addSeries(PPG_samples);
     }
 
     @Override
